4c7d7c3dcaba5c0548b7639b6fefe30b0e03b9e1
Franklin151515 <60908470@est.unap.edu.pe>
2026-09-10 09:51:52 -0500

    holaaaaa xd

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+void leerMatriz(int *A){
+    scanf();
+
+}
+
+int main()
+{
+    int Nv;
+    int Ne;
+    printf("BIENVENIDO A LA COMPUTADORA QUE RESUELVE TUS PROBLEMAS...");
+    printf("\n\nIngresa el numero de variables:");
+    scanf("%i",&Nv);
+    printf("\nIngresa el numero de ecuaciones:");
+    scanf("%i",&Ne);
+
+
+
+
+
+
+    leer();
+
+}
+
+
+
+
+
+
+
